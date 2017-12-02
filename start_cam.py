a96c1bd992c7fa0b56271e3b3b4f794d9e3fcb99
Rossi Alessandro <rossi111@unisi.it>
2017-12-02 12:53:33 +0100

    doubling window

--- start_cam.py
+++ start_cam.py
@@ -1,7 +1,10 @@
 import cv2
+import numpy as np
 
-cv2.namedWindow("preview")
+win_name = "preview"
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
 vc = cv2.VideoCapture(0)
+img_shape = (480, 640, 3)
 
 face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml')  # ubuntu
 
@@ -10,21 +13,36 @@ if vc.isOpened():  # try to get the first frame
 else:
     rval = False
 
+fms_counter = 0
+fms_rate = 3
+image = None
+detec = np.zeros(img_shape, dtype='unit8')
+
 while rval:
 
     rval, frame = vc.read()
     key = cv2.waitKey(20)
 
-    image = frame.copy()  # cv2.resize(frame.copy(), (240, 320))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if image is None:
+        image = frame.copy()
 
-    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    if fms_counter % fms_rate == 0:
+        image = frame.copy()  # cv2.resize(frame.copy(), (240, 320))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("preview", image)
+        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
+        if len(faces > 0):
+            detec = image.copy()
+        for (x, y, w, h) in faces:
+            cv2.rectangle(detec, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+    show = np.hstack((image, detec))
+    cv2.imshow(win_name, show)
+
+    fms_counter += 1
     if key == 27: # exit on ESC
         break
 
-cv2.destroyWindow("preview")
+    fms_counter = fms_counter%100
+
+cv2.destroyWindow(win_name)
